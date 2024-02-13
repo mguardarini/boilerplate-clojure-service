5828ea1aa87a770196da7453d3e5b9164b1494b5
Mauro Guardarini <mauro.guardarini@gmail.com>
2024-02-13 13:23:24 -0300

    update the project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject boilerplate "0.1.0-SNAPSHOT"
-  :description "This is a sample about use the component library from Stuart Sierra"
-  :url "http://example.com/FIXME"
+  :description "This is a boilerplate for clojure service"
+  :url "https://github.com/mguardarini/boilerplate-clojure-service"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
@@ -9,8 +9,6 @@
                  [com.stuartsierra/dependency "1.0.0"]
                  [com.stuartsierra/component "1.1.0"]
                  [ring/ring-defaults "0.3.2"]
-                 [ring/ring-mock "0.4.0"]
-                 [ring-mock "0.1.5"]
                  [prismatic/schema "1.4.1"]
                  [clj-http "3.12.3"]
                  [nubank/state-flow "5.14.5"]]
